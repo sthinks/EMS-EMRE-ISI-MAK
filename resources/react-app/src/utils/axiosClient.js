8e217b7a508f63d92111b5fc368cb76a2d1b7d3c
Onur Çöpoğlu <100149612+onurcpgl@users.noreply.github.com>
2023-03-28 14:34:25 +0300

    last version ems

--- resources/react-app/src/utils/axiosClient.js
+++ resources/react-app/src/utils/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const AxiosClient = axios.create({
-  baseURL: `http://127.0.0.1:90/api/`,
+  baseURL: `http://emremakina.com.tr/api/`,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
