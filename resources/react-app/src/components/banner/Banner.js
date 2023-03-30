ca2ccf678de0812f5c63f522735f670976e0a919
Onur Çöpoğlu <100149612+onurcpgl@users.noreply.github.com>
2023-03-30 09:00:39 +0300

    some change

--- resources/react-app/src/components/banner/Banner.js
+++ resources/react-app/src/components/banner/Banner.js
@@ -4,8 +4,12 @@ function Banner({ image, title, titleSecond }) {
   return (
     <div className="w-auto h-auto flex justify-start items-center relative">
       <div className="bg-black opacity-50 w-full h-full absolute" />
-      <img className="w-full" src={image} alt="" />
-      <div className="absolute border-l-4 border-l-[#fdab0c] text-white ml-28 max-md:ml-10 pt-5">
+      <img
+        className="w-full max-sm:h-[300px] max-sm:object-cover"
+        src={image}
+        alt=""
+      />
+      <div className="absolute border-l-4 border-l-[#fdab0c] text-white ml-28 max-md:ml-10 py-5">
         <p className="text-6xl max-xl:text-4xl max-md:text-2xl">{title}</p>
         {titleSecond && (
           <p className="text-7xl font-semibold max-xl:text-5xl max-md:text-2xl">
